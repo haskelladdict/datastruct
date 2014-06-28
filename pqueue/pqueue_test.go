e8a74aedaf28cf686baa61841d596b942aca5455
Markus Dittrich <haskelladdict@gmail.com>
2014-06-28 15:44:25 -0400

    Added unit test for priority queue.

--- pqueue/pqueue_test.go
+++ pqueue/pqueue_test.go
@@ -2,19 +2,45 @@
 package pqueue
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
+
+const maxint = 2147483647
 
 func Test_queue(t *testing.T) {
 
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	var items []*Item
-	one := Item{"one", 1, 0}
-	items = append(items, &one)
-
 	pq := New(items)
-	pq.Update(&one, "bar", 2)
 
-	if i := pq.Pop(); i.Val != "bar" {
-		t.Error("incorrect queue elements")
+	for i := 0; i < 1000000; i++ {
+		item := &Item{i, r.Int(), i}
+		pq.Push(item)
+		items = append(items, item)
 	}
+
+	val := -1
+	for i := 0; i < 1000000; i++ {
+		newVal := pq.Pop().Priority
+		if newVal < val {
+			t.Error("incorrect ordering in priority queue pq")
+		}
+	}
+
+	pq1 := New(items)
+	for i := 0; i < 1000000; i++ {
+		pq1.Update(items[i], r.Int(), i)
+	}
+
+	val = -1
+	for i := 0; i < 1000000; i++ {
+		newVal := pq1.Pop().Priority
+		if newVal < val {
+			t.Error("incorrect ordering in priority queue pq1")
+		}
+	}
+
 }
